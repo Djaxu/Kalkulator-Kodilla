4a197f5f63b7c581c1bfeb6323f4060ba9480352
Djaxu <koparr@gmail.com>
2017-11-28 09:47:02 +0100

    Zadanie z modółu 4 calculator

---
+++ src/Calculator.java
@@ -0,0 +1,11 @@
+public class Calculator {
+
+    public void someMethod() {
+        System.out.println("Test");
+    }
+    public static void main(String args[]) {
+        Calculator calculator;
+        calculator = new Calculator();
+        calculator.someMethod();
+    }
+}
